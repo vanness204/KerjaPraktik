023e68d6ba67503a2fc77439862a53cfaedf23f1
Vanness Bee <vannes.204@gmail.com>
2024-03-07 18:35:55 +0700

    breeze

--- KerjaPraktikVannessFadly/resources/views/layouts/navigation.blade.php
+++ KerjaPraktikVannessFadly/resources/views/layouts/navigation.blade.php
@@ -17,7 +17,7 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('notes')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('notes.index')" :active="request()->routeIs('notes.*')">
                         {{ __('Notes') }}
                     </x-nav-link>
                 </div>
