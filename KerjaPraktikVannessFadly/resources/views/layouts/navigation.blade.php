74b3c6405bc6a256082635ef30da3d1f5c952618
Vanness Bee <vannes.204@gmail.com>
2024-03-11 12:57:18 +0700

    update

--- KerjaPraktikVannessFadly/resources/views/layouts/navigation.blade.php
+++ KerjaPraktikVannessFadly/resources/views/layouts/navigation.blade.php
@@ -16,11 +16,6 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('notes.index')" :active="request()->routeIs('notes.*')">
-                        {{ __('Notes') }}
-                    </x-nav-link>
-                </div>
             </div>
 
             <!-- Settings Dropdown -->
--- KerjaPraktikVannessFadly/routes/web.php
+++ KerjaPraktikVannessFadly/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -27,8 +26,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    Route::resource('notes', NoteController::class);
 });
 
 require __DIR__.'/auth.php';
